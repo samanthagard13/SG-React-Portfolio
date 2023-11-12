5d04cf53fe341947dea2a0605befe93294205153
samantha gard <samantha.gard13@gmail.com>
2023-11-12 16:58:15 -0500

    added example slideshow'

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,35 @@
 import React from "react";
-import { Container, Carousel } from "react-bootstrap";
+import { Container, Carousel, Button } from "react-bootstrap";
 
 function Projects() {
   return (
     <Container className="d-flex justify-content-center align-items-center">
     <Carousel>
-      <Carousel.Item href="https://github.com/samanthagard13/tech-blog">
-        <img src="/images/tech-screenshot.png" text="Find It"></img>
+      <Carousel.Item>
+        <img src="/images/tech-screenshot.png"></img>
         <Carousel.Caption>
-          <h3>Tech Blog</h3>
+          <a href="https://github.com/samanthagard13/tech-blog" target="_blank" className="project-link">Tech Blog</a>
         </Carousel.Caption>
       </Carousel.Item>
 
-      <Carousel.Item href="https://github.com/samanthagard13/Weather-Where-You-Are">
-        <img src="/images/weather-site.png" text="Find It"></img>
+      <Carousel.Item>
+        <img src="/images/weather-site.png"></img>
         <Carousel.Caption>
-          <h3>Tech Blog</h3>
+          <a href="https://github.com/samanthagard13/Weather-Where-You-Are" target="_blank" className="project-link">Weather Where You Are</a>
+        </Carousel.Caption>
+      </Carousel.Item>
+e
+      <Carousel.Item>
+        <img src="/images/Headline-screenshot.png"></img>
+        <Carousel.Caption>
+          <a  href="https://github.com/eddyK15501/headline-report" target="_blank" className="project-link">Headline Report</a>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img src="/images/react-example.png"></img>
+        <Carousel.Caption>
+          <a href="https://github.com/samanthagard13/example-portfolio" target="_blank" className="project-link">Example React Portfolio</a>
         </Carousel.Caption>
       </Carousel.Item>
 
