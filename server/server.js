943c2e97be1b6d8a9bdc527ce9be2caf7ae41a42
samantha gard <samantha.gard13@gmail.com>
2023-11-14 10:37:56 -0500

    heroku bugs

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
