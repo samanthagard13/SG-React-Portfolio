f4ba03c7d767dbdea872355a6af909d43783d0a7
samantha gard <samantha.gard13@gmail.com>
2024-01-22 22:36:36 -0500

    added art to digital creations page

--- src/pages/ThreeDPage.jsx
+++ src/pages/ThreeDPage.jsx
@@ -7,32 +7,60 @@ function ThreeDPage() {
   return (
     <>
       <NavBar />
+
       <Container className="text-center" fluid>
         <Image src="/images/noarrow-3d.png" />
-
+        <h2>Blender</h2>
         <Row>
-          <Col xs={12} lg={12} xl={6}>
+          <Col>
             <Card className="threed-card">
-              <Card.Img src="/images/section1-render.png" className="art-img"/>
-              <Card.Title>Futuristic Studio Apartment</Card.Title>
-              <Card.Text>360 render created in Blender</Card.Text>
+              <Card.Img src="/images/iso-bedroom.png" className="iso"/>
+              <Card.Title>Isometric Bedroom</Card.Title>
             </Card>
           </Col>
-          
-          <Col xs={12} lg={12} xl={6}>
+        
+          <Col>
             <Card className="threed-card">
-              <Card.Img src="/images/modern-livingroom.png"  className="art-img"/>
+              <Card.Img
+                src="/images/modern-livingroom.png"
+                className="art-img"
+              />
+              <Card.Img src="/images/livingroom-closeup.png" className="art-img"/>
               <Card.Title>Modern Living Room</Card.Title>
-              <Card.Text>Created in Blender</Card.Text>
+            </Card>
+          </Col>
+        
+          <Col>
+            <Card className="threed-card">
+              <Card.Img src="/images/section1-render.png" className="art-img" />
+              <Card.Img src="/images/section1-closeup.png" className="art-img"/>
+              <Card.Title>Futuristic Studio Apartment 360</Card.Title>
             </Card>
           </Col>
         </Row>
+        <h2>Aseprite</h2>
         <Row>
-          <Col></Col>
-          <Col></Col>
-          <Col></Col>
+          <Col>
+            <Card className="threed-card">
+              <Card.Img
+                src="/images/test1-sprite-animation.gif"
+                className="gif"
+              />
+            </Card>
+          </Col>
+          <Col>
+            <Card className="threed-card">
+              <Card.Img src="/images/sprite.png" className="gif" />
+            </Card>
+          </Col>
+          <Col>
+            <Card className="threed-card">
+              <Card.Img src="/images/creepers.png" className="gif" />
+            </Card>
+          </Col>
         </Row>
-        <Footer/>
+
+        <Footer />
       </Container>
     </>
   );
