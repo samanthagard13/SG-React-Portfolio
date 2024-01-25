e6e858703eca1e2cecafb61694c9880d003c3e34
samantha gard <samantha.gard13@gmail.com>
2024-01-25 10:55:40 -0500

    changed about paragraph

--- src/components/blurb.jsx
+++ src/components/blurb.jsx
@@ -4,8 +4,7 @@ function Blurb() {
     return(
         <>
         <p className="p-4"> The Continual Blend of Art and Technology Drives My Passion for
-         Developing and Creating. Focusing on the Integration of Web Design, Pixel Art, 3D Art, 
-         and Game Development.
+         Developing and Creating. 
         </p>
         </>
     )
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -10,35 +10,41 @@ function AboutMe() {
       <NavBar />
       <Container style={{ marginTop: "2em" }} className="text-center" fluid>
         <Row>
-        <Col xl={6}>
+          <Col xl={6}>
             <Image src="/images/IMG_3577.jpg" className="about-pic" rounded />
           </Col>
-        <Col xl={6}>
+          <Col xl={6}>
             <p style={{ margin: "2em", padding: "2em" }} className="about-p">
-              Transitioning from the restaurant industry to full-stack
-              development, I've decided to explore a path that allows me to
-              integrate my passion for design and art with the endless world of
-              technology. Using the skills I've accumulated in my Full-Stack
-              Coding Certification to continue creating beautiful and productive
-              websites. My experiences in life have allowed me to develop
-              essential skills such as teamwork, adaptability, and customer
-              service. These attributes are now driving my transition into the
-              world of technology, coding, and creative problem-solving.
+              As an enthusiastic and newly certified IT professional with a Full
+              Stack Coding Certificate from the University of North Carolina, I
+              am eager to transition into the dynamic world of technology and
+              software development. My journey has honed critical skills such as
+              problem-solving, teamwork, adaptability, and customer service, all
+              of which are vital in the tech industry. With a strong foundation
+              in both front-end and back-end technologies, I aim to blend my
+              passion for design and art with the innovative realm of coding and
+              technology. My goal is to contribute creatively and technically to
+              projects, leveraging my skills to provide effective and
+              user-centric solutions in the ever-evolving IT landscape.
             </p>
           </Col>
-          
-          
         </Row>
-        <Row style={{ marginTop: "2em" }} >
+        <Row style={{ marginTop: "2em" }}>
           <Col xl={6}>
             <h2>Technical Skills</h2>
-            
-            <p style={{ margin: "2em", padding: "1em" }}>HTML | CSS | JavaScript | Handlebars | Bootstrap | API | MySql | Express | jQuery | Node.js | React </p>
+
+            <p style={{ margin: "2em", padding: "1em" }}>
+              HTML | CSS | JavaScript | Handlebars | Bootstrap | API | MySql |
+              Express | jQuery | Node.js | React{" "}
+            </p>
           </Col>
           <Col xl={6}>
             <h2>Proficient In</h2>
 
-            <p style={{ margin: "2em", padding: "1em" }}>Github | Git | Figma | Canva | Shopify | Blender | Godot | Aseprite</p>
+            <p style={{ margin: "2em", padding: "1em" }}>
+              Github | Git | Figma | Canva | Shopify | Blender | Godot |
+              Aseprite
+            </p>
           </Col>
         </Row>
 
